fb5bc7fdbb8ef7a085b18a619c67b1b083417c7d
Brian Wangila <bwanjalah@gmail.com>
2022-08-17 20:59:15 +0300

    done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,27 @@
 class Dog
 
+  #setter method
+  def name=(dog_name)
+    @name = dog_name
+  end
+
+  def breed=(dog_breed)
+    @breed = dog_breed
+  end
+
+  #getter method
+  def name
+    @name
+  end
+  
+  def breed
+    @breed
+  end
 end
+
+lessie = Dog.new
+lessie.name = "Lessie"
+lessie.breed = "Mastiff"
+
+puts lessie.name
+puts lessie.breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,27 @@
 class Person
 
+  #setter method
+  def name=(name)
+    @name = name
+  end
+
+  def job=(job_name)
+    @job = job_name
+  end
+
+  # getter method
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end
 end
+
+kanye = Person.new
+kanye.name = "Kanye"
+kanye.job = "musician"
+
+puts kanye.name
+puts kanye.job
